4d2648a648b85b0648bdd2d5a756de2d6ba40774
Nachi <nils.christian2006@gmail.com>
2024-11-21 17:03:15 +0100

    change lol

--- obj/Release/net8.0/linux-x64/Gameoflife.AssemblyInfo.cs
+++ obj/Release/net8.0/linux-x64/Gameoflife.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gameoflife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d480cff6969fcfe6ef0e2bb4c568802f67ee3323")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c27886a95e01154ff0ee690f88ea1acf101d336")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gameoflife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gameoflife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
