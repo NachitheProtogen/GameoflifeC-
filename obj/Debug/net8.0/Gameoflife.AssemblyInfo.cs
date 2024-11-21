322e373cb9d400b7e21bc7b6e343822aed3f055d
Nachi <nils.christian2006@gmail.com>
2024-11-21 16:18:34 +0100

    Release

--- obj/Debug/net8.0/Gameoflife.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gameoflife.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gameoflife")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+001fbe589221561b9056c0f6b55e114cd920260d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d480cff6969fcfe6ef0e2bb4c568802f67ee3323")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gameoflife")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gameoflife")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
